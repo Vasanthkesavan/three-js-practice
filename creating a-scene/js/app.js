2bfb823dc14455782f05f78d9a57d51d3f8e39e7
vasanth <vk432@njit.edu>
2017-07-14 22:46:41 -0400

    added init function to initialize the created code on load

--- creating a-scene/js/app.js
+++ creating a-scene/js/app.js
@@ -4,40 +4,42 @@
   3. Renderer
 */
 
-let scene = new THREE.Scene();
+var handleResize;
 
-let camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+function init() {
+  let scene = new THREE.Scene();
 
-let renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+  let camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
+  let renderer = new THREE.WebGLRenderer();
+  renderer.setClearColor(0x000000, 1.0);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMapEnabled = true;
+  document.body.appendChild( renderer.domElement );
 
-/* Now we have rendered a scene
-  1. Need a BoxGeometry which has all vertices and faces of the cube
-  2. Need a basic material to color the cube
-  3. Need a mesh to apply material to the geometry
-  4. Need a method call to add the mesh to the scene
-  5. Need a render method call to render it
-*/
+  const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  const cube = new THREE.Mesh( geometry, material );
+  scene.add( cube );
 
+  camera.position.z = 10;
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+  let animate = function () {
+    requestAnimationFrame( animate );
+    cube.rotation.x -= 0.1;
+    cube.rotation.y -= 0.1;
+    renderer.render(scene, camera);
+  };
 
-camera.position.z = 10;
+  handleResize = function() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
 
-let animate = function () {
-  requestAnimationFrame( animate );
-  cube.rotation.x -= 0.1;
-  cube.rotation.y -= 0.1;
-  renderer.render(scene, camera);
-};
+  animate();
+}
 
-animate();
+window.onload = init;
 
-
-
-
+window.addEventListener('resize', handleResize, false);
